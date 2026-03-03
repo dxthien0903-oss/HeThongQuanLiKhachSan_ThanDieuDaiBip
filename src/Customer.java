caf263b0c6706f396a54ba6cb7c32a1f55aaa04b
Đặng Xuân Thiện <dxthien0903@gmail.com>
2026-03-03 10:13:57 +0700

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,42 +1,21 @@
 package model;
 
-public class Customer {
-
+public class Customer extends Person {
     private int customerId;
-    private String fullName;
-    private String phone;
-    private String email;
     private String idCard;
 
-    public Customer(int customerId, String fullName, String phone, String email, String idCard) {
+    public Customer(int customerId, String fullName, String phone,
+                    String email, String idCard) {
+        super(fullName, phone, email);
         this.customerId = customerId;
-        this.fullName = fullName;
-        this.phone = phone;
-        this.email = email;
         this.idCard = idCard;
     }
 
-    public int getCustomerId() {
-        return this.customerId;
-    }
-
-    public String getFullName() {
-        return this.fullName;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getIdCard() {
-        return this.idCard;
-    }
-
-    public String getContactInfo() {
-        return this.fullName + " - " + this.phone + " - " + this.email;
+    @Override
+    public String toString() {
+        return "Customer | "
+                + super.getContactInfo()
+                + " | CustomerID: " + this.customerId
+                + " | IDCard: " + this.idCard;
     }
 }
